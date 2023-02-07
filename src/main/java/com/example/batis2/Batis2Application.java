eec050f276191c177122c2be1f8f17feee3a0ff5
dinhluyen17 <118953522+dinhluyen17@users.noreply.github.com>
2023-02-07 22:43:13 +0700

    firtst commit

---
+++ src/main/java/com/example/batis2/Batis2Application.java
@@ -0,0 +1,13 @@
+package com.example.batis2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batis2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Batis2Application.class, args);
+    }
+
+}
