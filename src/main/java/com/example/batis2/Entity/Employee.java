68e511d39832d9589631030c45d6e5cfd14b69c2
dinhluyen17 <118953522+dinhluyen17@users.noreply.github.com>
2023-02-07 23:54:49 +0700

    commit 2

---
+++ src/main/java/com/example/batis2/Entity/Employee.java
@@ -0,0 +1,15 @@
+package com.example.batis2.Entity;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Employee {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+}
+
