55a498e32d67cc0fbc70d0de5c0588a7cd9264e6
dinhluyen17 <luyendd@mz.co.kr>
2023-02-10 17:29:16 +0700

    aldf

--- src/main/java/com/example/batis2/EmpController.java
+++ src/main/java/com/example/batis2/EmpController.java
@@ -9,13 +9,14 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -47,6 +48,31 @@ public class EmpController {
 //            sqlSession.close();
 //        }
         return employeeMyBatisRepository.findAll();
+    }
+
+    @PostMapping(path = "/convertJsonToQasm")
+    public ResponseEntity<?> convert(@RequestBody String json) {
+        JSONObject json = new JSONObject(string);
+        System.out.println(json.toString());
+        String technology = json.getString("technology");
+        System.out.println(technology);
+
+        System.out.println(json);
+        RestTemplate restTemplate = new RestTemplate();
+        String requestUrl = "http://localhost:8000/json-to-qasm";
+        String demo = restTemplate.postForObject(requestUrl, json, String.class);
+        System.out.println(demo);
+        return ResponseEntity.ok(demo);
+    }
+
+    ArrayList<String> jsonStringToArray(String jsonString) throws JSONException {
+        ArrayList<String> stringArray = new ArrayList<String>();
+        JSONob jsonArray = new JSONArray(jsonString);
+        for (int i = 0; i < jsonArray.length(); i++) {
+            stringArray.add(jsonArray.getString(i));
+        }
+
+        return stringArray;
     }
 
 }
