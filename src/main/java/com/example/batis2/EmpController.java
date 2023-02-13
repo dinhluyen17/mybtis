dd9ecd91ee36ec6c28da426842d97ccf43a8c01c
dinhluyen17 <luyendd@mz.co.kr>
2023-02-13 17:53:08 +0700

    dao dinbh luyen

--- src/main/java/com/example/batis2/EmpController.java
+++ src/main/java/com/example/batis2/EmpController.java
@@ -16,10 +16,8 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -28,107 +26,70 @@ import java.util.stream.Collectors;
 public class EmpController {
     private final EmployeeMyBatisRepository employeeMyBatisRepository;
     private final EmpMapper empMapper;
+    private  final Service service;
 
     @GetMapping(path = "{id}")
     public Employee findbyid(@PathVariable Integer id) {
-//        SqlSession sqlSession = .getSqlSessionFactory().openSession();
-//        try{
-//            EmpMapper userMapper = sqlSession.getMapper(EmpMapper.class);
-//            return userMapper.findById(id);
-//        }finally{
-//            sqlSession.close();
-//        }
         return null;
     }
 
     @GetMapping(path = "")
     public List<Employee> getAll() {
-//        SqlSession sqlSession = MyBatisUtil.getSqlSessionFactory().openSession();
-//        try{
-//            EmpMapper userMapper = sqlSession.getMapper(EmpMapper.class);
-//            return userMapper.findAll();
-//        }finally{
-//            sqlSession.close();
-//        }
         return employeeMyBatisRepository.findAll();
     }
 
     @PostMapping(path = "/convertJsonToQasm")
     public ResponseEntity<?> convert(@RequestBody String json) {
-        JSONObject json = new JSONObject(string);
-        System.out.println(json.toString());
-        String technology = json.getString("technology");
-        System.out.println(technology);
-
-        System.out.println(json);
-        RestTemplate restTemplate = new RestTemplate();
-        String requestUrl = "http://localhost:8000/json-to-qasm";
-        String demo = restTemplate.postForObject(requestUrl, json, String.class);
+        String demo = service.getQasmCode(json);
         System.out.println(demo);
         return ResponseEntity.ok(demo);
     }
 
-    ArrayList<String> jsonStringToArray(String jsonString) throws JSONException {
-        ArrayList<String> stringArray = new ArrayList<String>();
-        JSONob jsonArray = new JSONArray(jsonString);
-        for (int i = 0; i < jsonArray.length(); i++) {
-            stringArray.add(jsonArray.getString(i));
-        }
-
-        return stringArray;
-    }
-
-    public List<List<Objects>> getJsonList(String json) {
-        String json2 = "{\"cols\":[[1,1,\"Z^½\"],[\"Z^¼\",\"Z^¼\"],[1,\"Swap\",\"Swap\"],[1,1,\"Measure\"]]}";
-        System.out.println(json2);
-        return null;
-    }
-
-    public static List<List<Objects>> getJsonList(String json) {
-        String json2 = "{\"cols\":[[1,{\"id\":\"Z^ft\",\"arg\":\"sin(pi)\"}]]}";
-        String json3 = json2.replace("\"", "");
-        String json4 = json3.substring(7, json3.length() - 2);
-
-        List<String> list = List.of(json4.split("],|]"));
-        List<String> list2 = list.stream().map(item -> item.replace("[", "")).collect(Collectors.toList());
-        List<List<String>> demo = new ArrayList<>();
-        for(String str : list2) {
-            if(!str.contains("arg")) {
-                List<String> col = List.of(str.split(","));
-                demo.add(col);
-            } else {
-                demo.add(Collections.singletonList(str));
-            }
-        }
-        System.out.println("demo request: " + demo.get(0));
-        getJsonRequest(demo);
-        return null;
-    }
-
-    public static void getJsonRequest(List<List<String>> list) {
-        String prefix = "{\"cols\":[";
-        String endfix = "]";
-
-        for (List<String> strings : list) {
-            StringBuilder url = new StringBuilder("[");
-            for (int j = 0; j < strings.size(); j++) {
-                if(!strings.get(j).equals("1")) {
-                    url.append("\"");
-                    url.append(strings.get(j));
-                    url.append("\"");
-                }
-                else {
-                    url.append(strings.get(j));
-                }
-                if(j < strings.size() - 1) {
-                    url.append(", ");
-                } else {
-                    url.append("]");
-                }
-            }
-            System.out.println(prefix + url + endfix);
-        }
-
-    }
+//    public static List<List<Objects>> getJsonList(String json) {
+//        String json2 = "{\"cols\":[[1,{\"id\":\"Z^ft\",\"arg\":\"sin(pi)\"}]]}";
+//        String json3 = json2.replace("\"", "");
+//        String json4 = json3.substring(7, json3.length() - 2);
+//
+//        List<String> list = List.of(json4.split("],|]"));
+//        List<String> list2 = list.stream().map(item -> item.replace("[", "")).collect(Collectors.toList());
+//        List<List<String>> demo = new ArrayList<>();
+//        for(String str : list2) {
+//            if(!str.contains("arg")) {
+//                List<String> col = List.of(str.split(","));
+//                demo.add(col);
+//            } else {
+//                demo.add(Collections.singletonList(str));
+//            }
+//        }
+//        System.out.println("demo request: " + demo.get(0));
+//        getJsonRequest(demo);
+//        return null;
+//    }
+//
+//    public static void getJsonRequest(List<List<String>> list) {
+//        String prefix = "{\"cols\":[";
+//        String endfix = "]";
+//
+//        for (List<String> strings : list) {
+//            StringBuilder url = new StringBuilder("[");
+//            for (int j = 0; j < strings.size(); j++) {
+//                if(!strings.get(j).equals("1")) {
+//                    url.append("\"");
+//                    url.append(strings.get(j));
+//                    url.append("\"");
+//                }
+//                else {
+//                    url.append(strings.get(j));
+//                }
+//                if(j < strings.size() - 1) {
+//                    url.append(", ");
+//                } else {
+//                    url.append("]");
+//                }
+//            }
+//            System.out.println(prefix + url + endfix);
+//        }
+//
+//    }
 
 }
---
+++ src/main/java/com/example/batis2/Service.java
@@ -0,0 +1,162 @@
+package com.example.batis2;
+
+import org.springframework.web.client.RestTemplate;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@org.springframework.stereotype.Service
+public class Service {
+    private final RestTemplate restTemplate;
+    private final String requestPythonUrl = "http://localhost:8000/json-to-qasm2";
+
+    public Service(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getQasmCode(String jsonString) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String prefix = "OPENQASM 2.0;\n" +
+                "include \"qelib1.inc\";\n";
+        System.out.println(prefix);
+        List<List<String>> jsonList = getJsonList(jsonString);
+        List<String> jsonRequest = getJsonRequest(jsonList);
+        System.out.println("------");
+        jsonRequest.forEach(System.out::println);
+        for (String request : jsonRequest) {
+            String encodedRequestJsonCols = encodeStringRequest(request);
+            System.out.println("request encoded " + encodedRequestJsonCols);
+            String result = restTemplate.postForObject(requestPythonUrl, encodedRequestJsonCols, String.class);
+            analysizeResult(result, jsonList.get(0));
+        }
+        return null;
+    }
+
+    public List<List<String>> getJsonList(String json) {
+        String json3 = json.replace("\"", "");
+        String json4 = json3.substring(7, json3.length() - 2);
+
+        List<String> list = List.of(json4.split("],|]"));
+        List<String> list2 = list.stream().map(item -> item.replace("[", "")).collect(Collectors.toList());
+        List<List<String>> demo = new ArrayList<>();
+        for(String str : list2) {
+            if(!str.contains("arg")) {
+                List<String> col = List.of(str.split(","));
+                demo.add(col);
+            } else {
+                List<String> colWithArgGates = List.of(str.replaceAll("\\{", "[").replaceAll("}", "]").split(",(?=\\[^,]+?:)"));
+                System.out.println("test value " + colWithArgGates);
+                System.out.println("test value size" + colWithArgGates.size());
+                demo.add(colWithArgGates);
+            }
+        }
+        return demo;
+    }
+    public List<String> getJsonRequest(List<List<String>> list) {
+        System.out.println("what is json string :" + list);
+        String prefix = "{\"cols\":[";
+        String endfix = "]}";
+        List<String> requestList = new ArrayList<>();
+        for (List<String> strings : list) {
+            StringBuilder url = new StringBuilder("[");
+            System.out.println("string ban dau dau: " + strings);
+            for (int j = 0; j < strings.size(); j++) {
+                System.out.println("log string get J: " + j + strings.get(j));
+                if(strings.get(j).startsWith("{id")) {
+                    System.out.println("da chay vao day tai j = " + j);
+                    for(int k = 0; k < strings.get(j).length() - 1; k++) {
+                        url.append(strings.get(j).charAt(k));
+                        if(strings.get(j).charAt(k) == '{'
+                                || strings.get(j).charAt(k + 1) == ':'
+                                || strings.get(j).charAt(k) == ':'
+                                || strings.get(j).charAt(k + 1) == 'a'
+                                || strings.get(j).charAt(k) == 'g'
+                                || strings.get(j).charAt(k + 1) == ','
+                                || strings.get(j).charAt(k + 1) == '}') {
+                            url.append("\"");
+                        }
+                    }
+                }
+                else if(!strings.get(j).equals("1")) {
+                    url.append("\"");
+                    url.append(strings.get(j));
+                    url.append("\"");
+                }
+                else {
+                    url.append(strings.get(j));
+                }
+
+                if(j < strings.size() - 1) {
+                    url.append(", ");
+                } else {
+                    url.append("]");
+                }
+            }
+            requestList.add(prefix + url + endfix);
+        }
+        return requestList;
+    }
+
+    public String encodeStringRequest(String stringRequest) {
+       return Base64.getEncoder().encodeToString(stringRequest.getBytes());
+    }
+
+    public StringBuilder analysizeResult(String rsColumn, List<String> jsonList) {
+        StringBuilder finalRs = new StringBuilder(); //save the return value
+
+        List<String> rsList = List.of(rsColumn.split("\\n\\n"));
+        List<String> finalRsWithNoIndex = List.of(rsList.get(2).replaceAll("\\n", "").split(";"));
+        List<String> finalRsNoIndex = new ArrayList<>();
+        //no need to handle result if rs column contain control gate (python backend has done it)
+        if(jsonList.contains("•")) {
+            for(int i = 2; i < rsList.size(); i++) {
+                finalRs.append(rsList.get(i));
+                finalRs.append("\n");
+            }
+            return finalRs;
+        }
+        for(String x : finalRsWithNoIndex) {
+//            System.out.println("check x" + x);
+            String gateName = x.replaceAll("\\s.*", "");
+            System.out.println(gateName);
+            finalRsNoIndex.add(gateName);
+        }
+        int idx = 0; //count index of gate in response list
+        int swapFlag = -1; //flag for the first time swap gate appear in the json column
+        int swapIdxCount = 0;
+        if(!jsonList.contains("Swap")) {
+            for(int i = 0; i < jsonList.size(); i++) {
+                if(!jsonList.get(i).equals("1")) {
+                    finalRsNoIndex.set(idx, finalRsNoIndex.get(idx) + " q[" + i + "];");
+                    idx++;
+                }
+            }
+        } else {
+            for(int i = 0; i < jsonList.size(); i++) {
+                if (jsonList.get(i).equals("Swap")) {
+                    if(swapFlag == -1) {
+                        swapFlag = idx;
+                    }
+                    swapIdxCount++;
+                    if(swapIdxCount < 2) {
+                        finalRsNoIndex.set(swapFlag, finalRsNoIndex.get(swapFlag) + " q[" + i + "],");
+                        idx++; //idx only increase one time only
+                    } else {
+                        finalRsNoIndex.set(swapFlag, finalRsNoIndex.get(swapFlag) + "q[" + i + "];");
+                    }
+                } else if(!jsonList.get(i).equals("1")) {
+                    finalRsNoIndex.set(idx, finalRsNoIndex.get(idx) + " q[" + i + "];");
+                    idx++;
+                }
+            }
+        }
+
+        //save rs to final string builder
+        for(String str : finalRsNoIndex) {
+            finalRs.append(str);
+            finalRs.append("\n");
+        }
+        System.out.println(finalRs);
+        return finalRs;
+    }
+}
